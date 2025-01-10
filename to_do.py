8a5b5019f633d536a11fccd59d22e4ac61291fe5
John <jgeiger@Johns-MacBook-Air.local>
2025-01-10 08:04:56 -0500

    final draft all working

--- to_do.py
+++ to_do.py
@@ -5,10 +5,10 @@ options = ['add', 'remove', 'view', 'quit']
 
 
 def add_item():
-    to_do_item = input('Please type the name of the item you would like to add. ')
+    to_do_item = input('Please type the name of the item you would like to add. ').title()
     if to_do_item in to_dos:
         print('This item is already in your to-do list. ')
-    else: 
+    else:
         to_dos.append(to_do_item.title())
 
 def remove_item():
@@ -18,20 +18,31 @@ def remove_item():
     else:
         to_dos.pop(item_to_remove - 1)
         print(f"Item number {item_to_remove} has been removed from your to-do list. ")
+    print('\n')
 
 def view_list():
-    if (len(to_dos) >=1):
+    if (len(to_dos) >= 1):
         for i, name in enumerate(to_dos):
             print(f"{i + 1} {name}")
+        print("\n")
     else:
         print("There are no items in your list.")
 
+def display_options():
+    print("Your options are: ")
+    for i, name in enumerate (options):
+        print(f"{i + 1}. {name}")
 
-prompt = 'Welcome to your to-do list!'
-prompt += '\nWould you like to add, remove, view or quit? '
 
-    
+def greeting ():
+    print('Welcome to your to-do list!')
+
+greeting()
+
+
 while True:
+    prompt = '\nPlease type the name of the option you would like to choose. '
+    display_options()
     answer = input(prompt)
     try: 
         if answer not in options:
@@ -39,12 +50,13 @@ while True:
         if (answer == 'add'):
             add_item()
         elif (answer == 'remove'):
+            view_list()
             remove_item()
         elif (answer == 'view'):
             print('Here is your list')
             view_list()
         elif (answer == 'quit'):
-            exit()
+            break
             
     except ValueError:
         print('Enter a valid value. ')
